68cf4b42934e2c1a119c4fdf588721acf46ded80
赵志刚 <834561898@qq.com>
2021-08-06 16:40:48 +0800

    项目初始化结构创建

---
+++ gulimall-ware/src/test/java/com/begin/gulimall/gulimallware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.begin.gulimall.gulimallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
